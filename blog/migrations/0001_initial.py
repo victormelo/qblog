23ff8a648d919fa0f8852d672ab9b9eeb0c943e6
victormelo <victorklbr@gmail.com>
2015-03-23 19:47:42 -0300

    Init

---
+++ blog/migrations/0001_initial.py
@@ -0,0 +1,31 @@
+# -*- coding: utf-8 -*-
+from __future__ import unicode_literals
+
+from django.db import models, migrations
+
+
+class Migration(migrations.Migration):
+
+    dependencies = [
+    ]
+
+    operations = [
+        migrations.CreateModel(
+            name='Entry',
+            fields=[
+                ('id', models.AutoField(serialize=False, auto_created=True, primary_key=True, verbose_name='ID')),
+                ('title', models.CharField(max_length=200)),
+                ('body', models.TextField()),
+                ('slug', models.SlugField(max_length=200, unique=True)),
+                ('publish', models.BooleanField(default=True)),
+                ('created', models.DateTimeField(auto_now_add=True)),
+                ('modified', models.DateTimeField(auto_now=True)),
+            ],
+            options={
+                'verbose_name_plural': 'Blog Entries',
+                'ordering': ['-created'],
+                'verbose_name': 'Blog Entry',
+            },
+            bases=(models.Model,),
+        ),
+    ]
